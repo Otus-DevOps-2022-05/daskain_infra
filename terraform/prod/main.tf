72082fdcde8c8a99d4b9089257fbbb589c628698
daskain <daskain@gmail.com>
2022-07-13 13:22:51 +0300

    add modules on terrafrom

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">= 0.13"
+#}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file

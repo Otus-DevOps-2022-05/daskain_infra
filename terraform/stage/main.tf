292f4ce8262e70a64901fd8a4898dc24163c7174
daskain <daskain@gmail.com>
2022-07-19 12:19:15 +0300

    HW №8 add ansible

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.76.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source = "yandex-cloud/yandex"
+#       version = "0.76.0"
+#     }
+#   }
+# }
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file

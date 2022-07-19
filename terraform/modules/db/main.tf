4fdb9bae1adfafe892c372b36a12a95ddeddee39
daskain <daskain@gmail.com>
2022-07-19 17:10:58 +0300

    upd .tf file

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,11 +1,11 @@
-#terraform {
+# terraform {
 #  required_providers {
 #    yandex = {
 #      source = "yandex-cloud/yandex"
 #    }
 #  }
 #  required_version = ">= 0.13"
-#}
+# }
 
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
